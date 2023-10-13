df316e86034497f72211f89bd93d2fa835149543
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-13 21:14:41 +0300

    add constant file

---
+++ src/constants/apiConst.js
@@ -0,0 +1,8 @@
+export const URL = `https://todo-redev.herokuapp.com/api`;
+
+export const options = {
+  headers: {
+    accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+};
